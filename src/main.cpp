d9d7a9a0904253311f8e3bcb8281e744f968b117
Dillon-ubuntu <dillon.chen1@gmail.com>
2020-05-14 20:50:22 +1000

    cpp is hard

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,78 @@
 #include <bits/stdc++.h>
-#include "tree.h"
-#include "round.h"
 
 using namespace std;
+
+class Claim {
+    public:
+    int num;
+    int rank;
+    int strength;
+    
+    Claim(int n, int r, int dice) {
+        num = n;
+        rank = r;
+        if (r!=1) {
+            strength = 5*n + (n/2) + r - 7;
+        } else {
+            if (r <= dice/2) {
+                strength = 11*n - 6;
+            } else {
+                strength = 5*dice + n - 1;
+            }
+        }
+    }
+
+    Claim() {}
+
+    // check if has greater strength
+    bool compare(Claim o) {
+        return strength >= o.strength;
+    }
+};
+
+class Node { // Game tree for player
+    public:
+    Node *prev;
+    bool root;
+    bool leaf;
+    Claim claim;
+
+    Node(Node *p) {
+        prev = p;
+        root = false;
+        leaf = false;
+    }
+
+    Node() {
+        root = true;
+    }
+};
+
+class Round {
+    public:
+    vector<int> p1dice;
+    vector<int> p2dice;
+    Node p1node;
+    Node p2node;
+
+    Round(int dice1, int dice2) {
+        srand (time(NULL));
+        p1dice.assign(6, 0);
+        p2dice.assign(6, 0);
+        for (int i = 0; i < dice1; i++) {
+            p1dice[rand()%6]++;
+        }
+        for (int i = 0; i < dice2; i++) {
+            p2dice[rand()%6]++;
+        }
+    }
+};
+    
 
 int main() {
     Round dong = Round(6,6);
     for (int i = 0; i < 6; i++) {
-        cout<<dong.getP1dice()[i]<<" ";
+        cout<<dong.p1dice[i]<<" ";
     }
     cout<<'\n';
 }
--- src/util.cpp
+++ src/util.cpp
@@ -1,47 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Claim {
-    public:
 
-    int num;
-    int rank;
-    int strength;
 
-    Claim(int n, int r, int dice) {
-        num = n;
-        rank = r;
-        if (r!=1) {
-            strength = 5*n + (n/2) + r - 7;
-        } else {
-            if (r <= dice/2) {
-                strength = 11*n - 6;
-            } else {
-                strength = 5*dice + n - 1;
-            }
-        }
-    }
+// int main() {
+//     int dice =4;
+//     for (int i = 1; i <= 6; i++) {
+//         for(int j = 2; j <= 6; j++) {
+//             Claim dong = Claim(i,j,dice);
+//             cout<<dong.num<<' '<<dong.rank<<' '<<dong.strength<<endl;
+//         }
+//         int j=1;
+//         Claim dong = Claim(i,j,dice);
+//         cout<<dong.num<<' '<<dong.rank<<' '<<dong.strength<<endl;
+//     }
 
-    // check if has greater strength
-    bool compare(Claim o) {
-        return strength >= o.strength;
-    }
-};
-
-int main() {
-    int dice =4;
-    for (int i = 1; i <= 6; i++) {
-        for(int j = 2; j <= 6; j++) {
-            Claim dong = Claim(i,j,dice);
-            cout<<dong.num<<' '<<dong.rank<<' '<<dong.strength<<endl;
-        }
-        int j=1;
-        Claim dong = Claim(i,j,dice);
-        cout<<dong.num<<' '<<dong.rank<<' '<<dong.strength<<endl;
-    }
-
-    cout<<'\n';
-    Claim dong =Claim(1,2,2);
-    Claim dong2 = Claim(1,1,2);
-    cout<<dong.compare(dong2)<<'\n';
-}
+//     cout<<'\n';
+//     Claim dong =Claim(1,2,2);
+//     Claim dong2 = Claim(1,1,2);
+//     cout<<dong.compare(dong2)<<'\n';
+// }
